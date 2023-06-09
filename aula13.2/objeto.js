835bf2bbfbe3117b5f40974f7a20d7c3051748c4
KarlaR <karlarobertati@gmail.com>
2023-06-09 17:12:27 -0300

    objeto-js

---
+++ aula13.2/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4899.00
+prod1.descontoLegal = 0.40
+
+console.log(prod1)
+
+const prod2 = {
+    nome:  'Camisa Polo',
+    preco: 79.90
+}
+
+console.log(prod2)
